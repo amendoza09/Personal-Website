56405ffa5ed46e8bf06312b093b77ed599a7cf0f
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-02-16 00:09:44 -0500

    mac-updates

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.onscroll = function() {
+    const abt = document.querySelector('#about');
+    const proj = document.querySelector('#projects');
+    const nav = document.querySelector('nav');
+    const a = document.querySelector('a');
+    
+    if ((abt.getBoundingClientRect().top <= 20) && (abt.getBoundingClientRect().bottom > 20)) {
+        nav.classList.add('light');
+        a.classList.add('light');
+    } else {
+        nav.classList.remove('light');
+        a.classList.remove('light');
+    }
+  }
